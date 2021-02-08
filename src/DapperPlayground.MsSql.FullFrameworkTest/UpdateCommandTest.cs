204ca2aab12c871bf76d05bbe2f3059a36a76f7f
Marko Marković <1478860+mmarkovic@users.noreply.github.com>
2021-02-08 13:56:16 +0100

    Dapper: Added UPDATE examples.

---
+++ src/DapperPlayground.MsSql.FullFrameworkTest/UpdateCommandTest.cs
@@ -0,0 +1,72 @@
+﻿namespace DapperPlayground.MsSql
+{
+    using System;
+    using System.Data;
+
+    using FluentAssertions;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class UpdateCommandTest : IDisposable
+    {
+        private readonly ITestOutputHelper outputHelper;
+        private readonly IDbConnection connection;
+        private readonly IDbTransaction transaction;
+        private readonly UpdateCommand testee;
+
+        public UpdateCommandTest(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+            this.connection = SqlServerConnectionFactory.OpenNew();
+            this.transaction = this.connection.BeginTransaction();
+            this.testee = new UpdateCommand(this.connection, this.transaction);
+        }
+
+        [Fact]
+        public void UpdatesCategory()
+        {
+            var category = new CategoryItem()
+            {
+                Id = 5,
+                Name = "Corn",
+                Description = "Medium Corn"
+            };
+
+            var rowsAffected = this.testee.UpdateCategory(category);
+
+            rowsAffected.Should().Be(1);
+        }
+
+        [Fact]
+        public void UpdatesCategories()
+        {
+            var categories = new[]
+            {
+                new CategoryItem
+                {
+                    Id = 5,
+                    Name = "Corn",
+                    Description = "Medium Corn"
+                },
+                new CategoryItem
+                {
+                    Id = 6,
+                    Name = "Meat",
+                    Description = "Substance"
+                }
+            };
+
+            var rowsAffected = this.testee.UpdateCategories(categories);
+
+            rowsAffected.Should().Be(2);
+        }
+
+        public void Dispose()
+        {
+            this.transaction.Rollback();
+            this.connection.Close();
+            this.connection.Dispose();
+        }
+    }
+}
---
+++ src/DapperPlayground/UpdateCommand.cs
@@ -0,0 +1,50 @@
+﻿namespace DapperPlayground
+{
+    using System.Collections.Generic;
+    using System.Data;
+
+    using Dapper;
+
+    public class UpdateCommand
+    {
+        private readonly IDbConnection connection;
+        private readonly IDbTransaction transaction;
+
+        public UpdateCommand(IDbConnection connection, IDbTransaction transaction)
+        {
+            this.connection = connection;
+            this.transaction = transaction;
+        }
+
+        public int UpdateCategory(CategoryItem category)
+        {
+            int rowsAffected = this.connection.Execute(
+                @"UPDATE dbo.Categories SET
+                     CategoryName = @name,
+                     Description = @description
+                  WHERE CategoryID = @id;",
+                category,
+                this.transaction);
+
+            return rowsAffected;
+        }
+
+        public int UpdateCategories(IEnumerable<CategoryItem> categories)
+        {
+            int rowsAffected = this.connection.Execute(
+                @"UPDATE dbo.Categories SET
+                     CategoryName = @name,
+                     Description = @description
+                  WHERE CategoryID = @id;",
+                categories,
+                this.transaction);
+
+            if (rowsAffected == 0)
+            {
+                throw new DBConcurrencyException("Row changed by someone else.");
+            }
+
+            return rowsAffected;
+        }
+    }
+}
