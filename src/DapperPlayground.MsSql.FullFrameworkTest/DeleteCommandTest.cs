d5460bd0a929edc9ac55f806e98ce6e84caec801
Marko Marković <1478860+mmarkovic@users.noreply.github.com>
2021-02-08 17:31:37 +0100

    Dapper: Added Delete examples.

---
+++ src/DapperPlayground.MsSql.FullFrameworkTest/DeleteCommandTest.cs
@@ -0,0 +1,41 @@
+﻿namespace DapperPlayground.MsSql
+{
+    using System;
+    using System.Data;
+
+    using FluentAssertions;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class DeleteCommandTest : IDisposable
+    {
+        private readonly ITestOutputHelper outputHelper;
+        private readonly IDbConnection connection;
+        private readonly IDbTransaction transaction;
+        private readonly DeleteCommand testee;
+
+        public DeleteCommandTest(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+            this.connection = SqlServerConnectionFactory.OpenNew();
+            this.transaction = this.connection.BeginTransaction();
+            this.testee = new DeleteCommand(this.connection, this.transaction);
+        }
+
+        [Fact]
+        public void UpdatesCategory()
+        {
+            var rowsAffected = this.testee.DeleteCategory(10);
+
+            rowsAffected.Should().Be(0);
+        }
+
+        public void Dispose()
+        {
+            this.transaction.Rollback();
+            this.connection.Close();
+            this.connection.Dispose();
+        }
+    }
+}
---
+++ src/DapperPlayground/DeleteCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace DapperPlayground
+{
+    using System.Data;
+
+    using Dapper;
+
+    public class DeleteCommand
+    {
+        private readonly IDbConnection connection;
+        private readonly IDbTransaction transaction;
+
+        public DeleteCommand(IDbConnection connection, IDbTransaction transaction)
+        {
+            this.connection = connection;
+            this.transaction = transaction;
+        }
+
+        public int DeleteCategory(int categoryId)
+        {
+            int rowsAffected = this.connection.Execute(
+                @"DELETE FROM dbo.Categories
+                  WHERE CategoryID = @id;",
+                new { id = categoryId },
+                this.transaction);
+
+            return rowsAffected;
+        }
+    }
+}
