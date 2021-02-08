119359294e94966cdcc44e0d7770f04ba8e7f61a
Marko Marković <1478860+mmarkovic@users.noreply.github.com>
2021-02-08 11:24:10 +0100

    Dapper: Added StoredProcedure by Query example.

---
+++ src/DapperPlayground.MsSql.FullFrameworkTest/StoredProceduresTest.cs
@@ -0,0 +1,44 @@
+﻿namespace DapperPlayground.MsSql
+{
+    using System;
+    using System.Data;
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class StoredProceduresTest : IDisposable
+    {
+        private readonly IDbConnection connection;
+        private readonly ITestOutputHelper outputHelper;
+        private readonly StoredProcedures testee;
+
+        public StoredProceduresTest(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+            this.connection = SqlServerConnectionFactory.OpenNew();
+            this.testee = new StoredProcedures(this.connection);
+        }
+
+        [Fact]
+        public void ExecutesStoredProcedures()
+        {
+            var result = this.testee.ExecuteSalesByCategory("Beverages", 1998).ToArray();
+
+            foreach (var row in result)
+            {
+                this.outputHelper.WriteLine($"{row.ProductName}: {row.TotalPurchase}");
+            }
+
+            result.Should().HaveCountGreaterThan(2);
+        }
+
+        public void Dispose()
+        {
+            this.connection.Close();
+            this.connection.Dispose();
+        }
+    }
+}
---
+++ src/DapperPlayground/StoredProcedures.cs
@@ -0,0 +1,25 @@
+﻿namespace DapperPlayground
+{
+    using System.Collections.Generic;
+    using System.Data;
+
+    using Dapper;
+
+    public class StoredProcedures
+    {
+        private IDbConnection connection;
+
+        public StoredProcedures(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public IEnumerable<dynamic> ExecuteSalesByCategory(string categoryName, int year)
+        {
+            return this.connection.Query(
+                "dbo.SalesByCategory",
+                new { CategoryName = categoryName, OrdYear = year },
+                commandType: CommandType.StoredProcedure);
+        }
+    }
+}
